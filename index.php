3d2fa5c239936c6cfa8a4ff12901d5cbb9de9929
messiJod <messi162108@gmail.com>
2022-05-16 18:32:38 +0530

    __destruct

--- index.php
+++ index.php
@@ -1,42 +1,25 @@
 <?php
 
-interface myInterface{
-    public function one();
-    public function two($a,$b);
-}
-
-class A implements myInterface{
-//    public $number1;
-//    public $number2;
-
-    public function one()
-    {
-        echo "Function one <br>";
+class A{
+    public function __construct(){
+        echo "This is construction function!<br>";
     }
-
-    public function two($a, $b)
-    {  $sum = $a+$b;
-        echo "The sum of $a and $b is ". $sum ."<br>" ;
-
+    public function SayHello(){
+        echo "Hello everyone!<br>";
+    }
+    public function __destruct(){
+        echo "Destruction function Called!!<br>";
     }
 }
 
-class B implements myInterface{
-    public function one()
-    {
-       echo "Class B ka one Function <br>";
-    }
-    public function two($a, $b)
-    {
-        echo "Class B ka function Two <br>";
-    }
-}
 $obj = new A();
 
-$obj->two(52,10);
+$number = 2;
 
-$obj2 = new B();
+for($i=0;$i<$number;$i++)
+{
+   $obj->SayHello();
+}
 
-$obj2->one();
-$obj2->two(10,55);
+echo "done<br>";
 
